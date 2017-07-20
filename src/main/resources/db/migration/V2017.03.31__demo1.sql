58eb6fa275207bbff24d9c94d6cafc51de2ca1ba
hanpenghu <563909408@qq.com>
2017-07-20 17:19:30 +0800

    2017_7_20   weekday(4)   17:19:24

--- src/main/resources/db/migration/V2017.03.31__demo1.sql
+++ src/main/resources/db/migration/V2017.03.31__demo1.sql
@@ -1,6 +1,6 @@
-create table demo(
-    id int primary key auto_increment,
-    name varchar(16)
-);
-insert INTO demo(name) VALUES ('a');
-insert INTO demo(name) VALUES ('b');
+-- create table demo(
+--     id int primary key IDENTITY,
+--     name varchar(16)
+-- );
+-- insert INTO demo(name) VALUES ('a');
+-- insert INTO demo(name) VALUES ('b');
